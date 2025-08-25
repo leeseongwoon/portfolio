1588dc1197a031ca76ecb67b03f2dd3097624bb3
leeseongwoon <156057545+leeseongwoon@users.noreply.github.com>
2025-08-25 15:34:27 +0900

    home motion 수정

--- src/styles/HomeSection.ts
+++ src/styles/HomeSection.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { typing, blink, fadeInUp } from "./CommonStyles";
+import { fadeInUp } from "./CommonStyles";
 
 //section
 export const HomeSection = styled.section`
@@ -16,17 +16,13 @@ export const HomeSection = styled.section`
   }
 `;
 // 타이핑 애니메이션 텍스트
-export const TypingText = styled.div`
+export const TypingText = styled.p`
   font-size: 1.4rem;
   color: #38bdf8;
   font-weight: 500;
   margin-bottom: 1rem;
   overflow: hidden;
   white-space: nowrap;
-  border-right: 3px solid #38bdf8;
-  animation: ${typing} 4s steps(25, end), ${blink} 1s step-end infinite;
-  width: 0;
-  animation-fill-mode: forwards;
   text-shadow: 0 0 10px rgba(56, 189, 248, 0.5);
 `;
 
